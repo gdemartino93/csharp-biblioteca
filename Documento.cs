4f5a4d5693f724a4b07dd9a25ea902f13d0550eb
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-01 15:53:25 +0200

    class document

---
+++ Documento.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Documento
+    {
+        private string codice;
+        private string titolo;
+        private DateTime anno;
+        private string settore;
+        private string stato;
+        private string scaffale;
+        private string autore;
+
+        public string Codice { get; set; }
+        public string Titolo { get; set; }
+        public DateTime Anno { get; set; }
+        public string Settore { get; set; }
+        public string Stato { get; set; }
+        public string Scaffale { get; set; }
+        public string Autore { get; set; }
+
+    }
+}
