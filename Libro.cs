7ab9b346509a808db95bbe7c2f9ca31c79bb7103
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-01 15:54:43 +0200

    class libro extended

---
+++ Libro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Libro : Documento
+    {
+        private int numPagine;
+        public int NumPagine { get; set; }
+
+        public Libro(int numPagine)
+        {
+            NumPagine = numPagine;
+        }
+    }
+}
