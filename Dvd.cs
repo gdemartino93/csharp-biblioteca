31b2220f52e65e6f12abe2c07ef31de7f095405c
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-01 16:07:32 +0200

    create dvd class with constructor

---
+++ Dvd.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Dvd : Documento
+    {
+        private int durata;
+        public int Durata { get; set; }
+
+        public Dvd(string codice,string titolo,int anno,string settore,string stato,string scaffale,string autore, int durata) : base(codice, titolo, anno, settore, stato, scaffale, autore)
+        {
+            Durata = durata;
+        }
+    }
+}
