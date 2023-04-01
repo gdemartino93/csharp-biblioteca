ddc92620bfcc73bd5b453b8ba02341e5545c89cf
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-01 16:28:05 +0200

    create prestito class with tostring method

---
+++ Prestito.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Prestito
+    {
+        private string prestitoDa;
+        private string prestitoA;
+        private Cliente cliente;
+        private Documento documento;
+
+        public string PrestitoDa { get; set; }
+        public string PrestitoA { get; set; }
+        public Cliente Cliente { get; set; }
+        public Documento Documento { get; set; }
+
+        public Prestito(string prestitoDa,string prestitoA,Documento documento, Cliente cliente)
+        {
+            PrestitoDa = prestitoDa;
+            PrestitoA = prestitoA;
+            Documento = documento;
+            Cliente = cliente;
+        }
+        public override string ToString()
+        {
+            return "dal: " + PrestitoDa + " al: " + PrestitoA + "In prestito a: " + Cliente.Nome + " " + Cliente.Cognome + " per " + Documento.Titolo;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@
             Documento libro = new Libro("asd", "asd",1990,"settore","disponibieò","scaffale","autore",5);
             Documento dvd = new Dvd("codicedv", "titolodvd", 1993, "settoredvd", "statodv", "scaffdvd", "autodvd", 190);
             Cliente cliente = new Cliente("marco", "rosis", "asdasdasd@gmail.com", "3209090901");
+
         }
     }
 }
