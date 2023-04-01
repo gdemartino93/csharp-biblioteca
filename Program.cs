626af7841c7575be2768775ff921188fb40ff48c
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-01 16:09:44 +0200

    create 3 instance

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Documento x = new Libro("asd", "asd",1990,"settore","disponibieò","scaffale","autore",5);
+            Documento libro = new Libro("asd", "asd",1990,"settore","disponibieò","scaffale","autore",5);
+            Documento dvd = new Dvd("codicedv", "titolodvd", 1993, "settoredvd", "statodv", "scaffdvd", "autodvd", 190);
+            Cliente cliente = new Cliente("marco", "rosis", "asdasdasd@gmail.com", "3209090901");
         }
     }
 }
